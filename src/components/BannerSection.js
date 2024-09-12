0db0ce3c3b9dbce0526ece85108574e94eb9e402
MatiasCeb <60543600+MatiasCeb@users.noreply.github.com>
2024-09-11 22:19:00 -0300

    banner text

--- src/components/BannerSection.js
+++ src/components/BannerSection.js
@@ -8,9 +8,9 @@ const BannerSection = () => {
         <div id="BannerSection" className="banner-section--container">
             {/* <div className="header--img" alt="" /> */}
             <div className="banner-section--title-container">
-                <h1 className="header--title-h1">Imagine what you could do with your life if you believed in yourself?</h1>
+                <h1 className="header--title-h1">Imagina lo que podrías ser ahora.</h1>
                 <BannerSep />
-                <p className="header--title-p">Let me show you the path that will lead you toward the dream you imagine. Better yet, choose the path (or paths) that works best for the life you want to live.</p>
+                <p className="header--title-p">Quiero compartirte una forma distinta de ver la vida que te rodea, en presencia y armonía.</p>
             </div>
         </div>
     )
